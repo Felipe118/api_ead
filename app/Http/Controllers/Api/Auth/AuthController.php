94dd2dee372ea7d3e909859b7fa2da64b271d9ea
Felipe118 <lipebezerraa@gmail.com>
2022-08-24 23:43:31 -0300

    feat: criando a autenticação

---
+++ app/Http/Controllers/Api/Auth/AuthController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers\Api\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\AuthRequest;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function auth(AuthRequest $request)
+    {
+        $user = User::where('email', $request->email)->first();
+ 
+        if (! $user || ! Hash::check($request->password, $user->password)) {
+            throw ValidationException::withMessages([
+                'email' => ['The provided credentials are incorrect.'],
+            ]);
+        }
+
+        return response()->json([
+            'token' => $user->createToken($request->device_name)->plainTextToken,
+        ]);
+
+
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\Auth\AuthController;
 use App\Http\Controllers\Api\LessonController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -7,6 +8,9 @@ use App\Http\Controllers\Api\CourseController;
 use App\Http\Controllers\Api\ModuleController;
 use App\Http\Controllers\Api\SupportController;
 use App\Http\Resources\ReplySupportResource;
+
+//
+Route::post('/auth', [AuthController::class, 'auth']);
 
 Route::get('/courses',[CourseController::class, 'index']);
 Route::get('/courses/{id}',[CourseController::class, 'show']);
