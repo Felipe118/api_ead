05504a2b054948b5dbb6d2ad156ba859e85c7f09
Felipe118 <lipebezerraa@gmail.com>
2022-09-08 23:42:04 -0300

    feat: finalizando a feature que usuario marca uma aula com visualizada

--- app/Repository/LessonRepository.php
+++ app/Repository/LessonRepository.php
@@ -3,9 +3,11 @@
 namespace App\Repository;
 
 use App\Models\Lesson;
+use App\Repository\Traits\RepositoryTrait;
 
 class LessonRepository
 {
+    use RepositoryTrait;
     protected $entity;
     
     public function __construct(Lesson $lesson)
@@ -19,8 +21,24 @@ class LessonRepository
     }
   
 
-    public function getLesson(string $id)
+    public function getLesson(string $id) : Lesson
     {
         return $this->entity->findOrFail($id);
+    }
+
+    public function markLessonViewed(string $lessonId)
+    {
+        $user = $this->getUserAuth();
+
+        $view = $user->views()->where('lesson_id',$lessonId)->first();
+
+        if($view){
+           return $view->update(['quantity'=>$view->quantity + 1]);
+        }
+        
+        return $user->views()->create([
+            'lesson_id' => $lessonId,
+            'quantity' => 1
+        ]);
     }
 }
