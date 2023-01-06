ebfd5605fb8120dd613056a121d222821276297d
Felipe118 <lipebezerraa@gmail.com>
2023-01-05 21:52:44 -0300

    fix

--- app/Http/Requests/AuthRequest.php
+++ app/Http/Requests/AuthRequest.php
@@ -26,7 +26,7 @@ class AuthRequest extends FormRequest
         return [
             'email' => 'required|email',
             'password' => 'required',
-            'device_name' => 'required',
+            // 'device_name' => 'required',
         ];
     }
 }
